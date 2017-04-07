56185ea76a5f3fe28a6179cfa7f2b81fae8a5c91
Unknown <cfranklin8@mail.csuchico.edu>
2017-04-06 17:13:19 -0700

    created randomly generated sentences

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,21 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+  var intro = ["Don't you think that","I think I heard that","I'm pretty sure that",
+  "Sometimes it seems like"];
+  var body = ["your mom","Pauly Shore","my dog","the President","my neighbor"];
+  var end = ["needs to chill out.","might be the Loch Ness monster.",
+  "has some real issues."];
+
+  randIndex = randomUpTo(intro.length-1);
+  sentence = intro[randIndex];
+  randIndex = randomUpTo(body.length-1);
+  sentence += " " + body[randIndex];
+  randIndex = randomUpTo(end.length-1);
+  sentence += " " + end[randIndex];
+
+  document.getElementById("tweet").innerHTML=sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
